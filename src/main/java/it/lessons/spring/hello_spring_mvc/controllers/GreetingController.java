7b0b568e25b5c22e033734bd2efc5ab17b3e1767
Giuseppe Pedullà <peduz91@gmail.com>
2025-03-25 21:09:23 +0100

    First commit

---
+++ src/main/java/it/lessons/spring/hello_spring_mvc/controllers/GreetingController.java
@@ -0,0 +1,26 @@
+package it.lessons.spring.hello_spring_mvc.controllers;
+
+import java.time.LocalTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class GreetingController {
+
+
+  @GetMapping("/greeting")
+  public String greeting(@RequestParam(name = "name") String name,
+    Model model) {
+    model.addAttribute("name", name);
+    model.addAttribute("time", LocalTime.now());
+
+    return "greeting";
+  }
+  
+
+}
